ddbaabccebbfc8f2de4683e717d7d80790b52f4a
Arun Regmi <aregmi450@gmail.com>
2022-03-15 21:03:24 +0545

    minor changes to the code

--- Calendar.tsx
+++ Calendar.tsx
@@ -1,22 +1,24 @@
 import React from "react";
-import {Text, View, StyleSheet} from 'react-native';
-import Calendar from 'react-native-calendar-range-picker';
+import { Text, View, StyleSheet } from "react-native";
+import Calendar from "react-native-calendar-range-picker";
 
 const UsedCalendar = () => {
-    return (
-        <View>
-            <Calendar
-                startDate='2021-12-14'
-                endDate='2021-12-23'
-                onChange={({ startDate, endDate }) => console.log({ startDate, endDate })}
-                style={{
-                    todayColor: 'blue',
-                    selectedDayBackgroundColor: '#FFD42A',
-                    selectedBetweenDayBackgroundTextColor: '#ffe58033'
-                }}
-            />
-        </View>
-    )
-}
+  return (
+    <View>
+      <Calendar
+        startDate="2022-03-11"
+        endDate="2021-12-23"
+        onChange={({ startDate, endDate }) =>
+          console.log({ startDate, endDate })
+        }
+        style={{
+          todayColor: "blue",
+          selectedDayBackgroundColor: "#FFD42A",
+          selectedBetweenDayBackgroundTextColor: "#ffe58033",
+        }}
+      />
+    </View>
+  );
+};
 
 export default UsedCalendar;
